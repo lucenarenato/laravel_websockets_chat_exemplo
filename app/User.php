153067c77515ab68a2df72c499a1370a0964f9ba
Renato de Oliveira Lucena <cpdrenato@gmail.com>
2020-07-03 19:26:03 -0300

    first websocket

---
+++ app/User.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App;
+
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class User extends Authenticatable
+{
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    public function messages()
+    {
+        return $this->hasMany(Message::class);
+    }
+
+    public function accounts()
+    {
+        return $this->hasMany('App\SocialAccount');
+    }
+
+    public function posts()
+    {
+        return $this->hasMany('App\Post');
+    }
+
+    public function comments()
+    {
+        return $this->hasMany('App\Comment');
+    }
+}
---
+++ database/seeds/DatabaseSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // $this->call(UsersTableSeeder::class);
+        factory(App\User::class, 25)->create()->each(function ($u) {
+            for ($i = 0; $i <= 3; $i++) {
+                $u->posts()->save(factory(App\Post::class)->make());
+            }
+        });
+    }
+}
